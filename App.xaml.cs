25bcfb4c69d4408e4ba30fa8ba56d1edf2f644d8
Felipe <Felipe@PC-FELIPE>
2026-08-08 00:26:14 -0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Main_Window
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Main_Window
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LaunchFirefox_Click(object sender, RoutedEventArgs e) =>
+            OpenApp(@"C:\Program Files\Mozilla Firefox\firefox.exe");
+
+        private void LaunchSteam_Click(object sender, RoutedEventArgs e) =>
+            OpenApp(@"C:\Program Files (x86)\Steam\steam.exe");
+
+        private void LaunchDiscord_Click(object sender, RoutedEventArgs e) =>
+            OpenApp(@"C:\Users\selho\AppData\Local\Discord\app-1.0.9251\Discord.exe");
+
+        private void Configs_Click(object sender, RoutedEventArgs e) =>
+            OpenApp(@"C:\Users\selho\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup");
+
+        private void OpenApp(string path)
+        {
+            try
+            {
+                ProcessStartInfo startInfo = new()
+                {
+                    FileName = path,
+                    UseShellExecute = true
+                };
+                Process.Start(startInfo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to open application: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+    }
+}

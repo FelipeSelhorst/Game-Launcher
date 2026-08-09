03e559ce90097e843fb7b1cbb4868f86ad722378
Felipe <selhorstdelimafelipe@gmail.com>
2026-08-09 15:47:21 -0300

    Added an animation to the buttons and started the home section. Also changed "Group 1: Home and Library" buttons to RadioButton, and limited the selection indicator bar to this group. Created new style effects for the main buttons (on the Home section) and added the page title and profile picture.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel;
+using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -6,6 +7,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,10 +22,23 @@ namespace Main_Window
         public MainWindow()
         {
             InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            MoveSelection(HomeButton, false);
         }
 
         private void LaunchFirefox_Click(object sender, RoutedEventArgs e) =>
             OpenApp(@"C:\Program Files\Mozilla Firefox\firefox.exe");
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e) =>
+            MoveSelection(HomeButton);
+
+        private void LibraryButton_Click(object sender, RoutedEventArgs e) =>
+            MoveSelection(LibraryButton);
 
         private void LaunchSteam_Click(object sender, RoutedEventArgs e) =>
             OpenApp(@"C:\Program Files (x86)\Steam\steam.exe");
@@ -32,7 +47,7 @@ namespace Main_Window
             OpenApp(@"C:\Users\selho\AppData\Local\Discord\app-1.0.9251\Discord.exe");
 
         private void Configs_Click(object sender, RoutedEventArgs e) =>
-            OpenApp(@"C:\Users\selho\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup");
+            OpenApp(@"C:\Program Files\WindowsApps\Microsoft.WindowsNotepad_11.2605.34.0_x64__8wekyb3d8bbwe\Notepad\Notepad.exe");
 
         private void OpenApp(string path)
         {
@@ -51,5 +66,49 @@ namespace Main_Window
             }
         }
 
+        private void CloseAppButton(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MoveSelection(FrameworkElement button, bool animate = true)
+        {
+            Point buttonPosition = button.TranslatePoint(
+                new Point(
+                    button.ActualWidth / 2,
+                    button.ActualHeight / 2
+                ),
+                Sidebar
+            );
+
+            double newPosition =
+                buttonPosition.Y - (SelectionIndicator.ActualHeight / 2);
+
+            if (!animate)
+            {
+                SelectionTransform.BeginAnimation(
+                    TranslateTransform.YProperty,
+                    null
+                );
+
+                SelectionTransform.Y = newPosition;
+                return;
+            }
+
+            var animation = new DoubleAnimation
+            {
+                To = newPosition,
+                Duration = TimeSpan.FromMilliseconds(200),
+                EasingFunction = new QuadraticEase
+                {
+                    EasingMode = EasingMode.EaseOut
+                }
+            };
+
+            SelectionTransform.BeginAnimation(
+                TranslateTransform.YProperty,
+                animation
+            );
+        }
     }
 }
